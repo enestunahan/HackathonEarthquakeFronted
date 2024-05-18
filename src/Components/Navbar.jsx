1cca52b1b431a08ecc88efeb0dca4f1490d66e1b
Enes Tunahan Çömez <enesstunahan@gmail.com>
2024-05-18 19:54:39 +0300

    navbar component created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='links'>
+            <ul>
+                <li>Ana Sayfa</li>
+                <li>Hakkımızda</li>
+                <li>Projelerimiz</li>
+                <li>Tecrübeler</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
