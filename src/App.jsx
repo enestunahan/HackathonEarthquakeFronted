7f55acd37402ca2fb990d01e176ce39e10b0a428
Enes Tunahan Çömez <enesstunahan@gmail.com>
2024-05-19 00:26:44 +0300

    information message komponenti düzenlendi , ikinci uyarı mesajı bilgileri gönderildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import Hero from './Components/Hero/Hero'
 import InformationMessage from './Components/InformationMessage/InformationMessage'
 import Navbar from './Components/Navbar/Navbar'
+import earthquakeBag from  './assets/images/depremcantasi.jpg'
+import meetingPlace from './assets/images/toplanmayeri.jpg'
 
 function App() {
+
+  const[earthquakeBagMessage,setEarthquakeBagMessage] = useState("Deprem çantası, özellikle afetten sonraki ilk 72 saat içerisinde kendinizin ve sevdiklerinizin acil ihtiyaçlarını temin edebilmeniz için gerekli. Deprem sonrası arama ve kurtarma faaliyetleri başlayana kadar acil ihtiyaçlarınızı hazırladığınız deprem çantasındaki ürünlerle giderebilirsiniz.");
+  const[earthquakeBagTitle,setEarthquakeBagTitle] = useState("Deprem Çantası Önemi");
+  const[meetingPlaceTitle, setMeetingPlaceTitle] = useState("Toplanma Yeri Önemi");
+  const[meetingPlaceMessage, setMeetingPlaceMessage] = useState("Yaşanan depremlerin ardından vatandaşlar paniğe kapılabilmektedir. Deprem sonrasında halk arasındaki paniğin önlenmesi amacıyla kurulan deprem toplanma alanları, sağlıklı bilgi alışverişinin sağlanmasını sağlamaktadır. Vatandaşların depremin ardından kendilerine en yakın acil toplanma yerinde beklemeleri gerekmektedir.");
 
   return (
     <>
         <Navbar/>
         <Hero/>
-        <InformationMessage/>
+        <InformationMessage image={earthquakeBag} title = {earthquakeBagTitle} message = {earthquakeBagMessage} className = "informationMessageContentearthquakeBag"/>
+        <InformationMessage image={meetingPlace} title = {meetingPlaceTitle} message = {meetingPlaceMessage} className = "informationMessageContentmeetingPlace"/>
     </>
   )
 }
--- src/Components/InformationMessage/InformationMessage.jsx
+++ src/Components/InformationMessage/InformationMessage.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import './InformationMessage.css'
-import earthquakeBag from '../../assets/images/depremcantasi.jpg'
 
-export default function InformationMessage() {
+export default function InformationMessage(props) {
   return (
-    <div className='informationMessage'>
+    <div  className = {`informationMessage ${props.className}`} >
         <div className='informationMessageContent'>
             <div className='informationMessageTitle'>
-                <h1> Deprem Çantası Önemi </h1>            
+                <h1> {props.title} </h1>            
             </div>
             <div className='informationMessageBody'>
-                 <img src={earthquakeBag} alt="" />
-                 <p>Deprem çantası, özellikle afetten sonraki ilk 72 saat içerisinde kendinizin ve sevdiklerinizin acil ihtiyaçlarını temin edebilmeniz için gerekli. Deprem sonrası arama ve kurtarma faaliyetleri başlayana kadar acil ihtiyaçlarınızı hazırladığınız deprem çantasındaki ürünlerle giderebilirsiniz.</p>
+                 <img src={props.image} alt="" />
+                 <p>{props.message}</p>
             </div>
         </div>
     </div>
