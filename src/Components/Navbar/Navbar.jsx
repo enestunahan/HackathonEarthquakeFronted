6e7314f18e7a5f2c0ef42b80ef596febda57acbf
Enes Tunahan Çömez <enesstunahan@gmail.com>
2024-05-19 00:49:20 +0300

    footer komponenti eklendi

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
+  
+  const[sticky , setSticky] = useState(false);
+  
+  useEffect(()=> {
+      window.addEventListener("scroll", ()=> {
+        window.scrollY > 50 ? setSticky(true) : setSticky(false);
+      })
+  },[])
+  
   return (
-    <div className='navbar'>
+    <div className= {`navbar ${sticky ? 'darkNav' : ''}`}>
         <div className='links'>
             <ul>
                 <li>Ana Sayfa</li>
